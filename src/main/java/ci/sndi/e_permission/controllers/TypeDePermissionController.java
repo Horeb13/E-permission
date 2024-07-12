b40deae9ba74afb030569bb7066274a41128ebe7
Bl@ck_C@im@n <yanndeliassanvo@gmail.com>
2024-07-12 12:28:29 +0000

    Fix type de permission controller

--- src/main/java/ci/sndi/e_permission/controllers/TypeDePermissionController.java
+++ src/main/java/ci/sndi/e_permission/controllers/TypeDePermissionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/typedpermissions")
+@RequestMapping("/api/typedepermissions")
 public class TypeDePermissionController {
 
     private final TypeDePermissionService typeDePermissionService;
